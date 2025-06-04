678cd8e86b7118bca6a67058640f028388e7221e
David Thorpe <djt@mutablelogic.com>
2025-06-04 20:36:11 +0200

    Updated

--- cmd/whisper/transcribe.go
+++ cmd/whisper/transcribe.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"time"
 
 	// Packages
 	segmenter "github.com/mutablelogic/go-media/pkg/segmenter"
 	whisper "github.com/mutablelogic/go-whisper"
+	client "github.com/mutablelogic/go-whisper/pkg/client"
 	schema "github.com/mutablelogic/go-whisper/pkg/schema"
 	task "github.com/mutablelogic/go-whisper/pkg/task"
 
@@ -23,6 +25,7 @@ type TranslateCmd struct {
 	Path     string        `arg:"" help:"Path to audio file"`
 	Format   string        `flag:"" help:"Output format" default:"text" enum:"json,verbose_json,text,vtt,srt"`
 	Segments time.Duration `flag:"" help:"Segment size for reading audio file"`
+	Api      bool          `flag:"api" help:"Use API for translation" default:"false"`
 }
 
 type TranscribeCmd struct {
@@ -31,17 +34,32 @@ type TranscribeCmd struct {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// GLOBALS
+
+const (
+	remoteUrl = "https://api.openai.com/"
+)
+
+////////////////////////////////////////////////////////////////////////////////
 // PUBLIC METHODS
 
 func (cmd *TranscribeCmd) Run(app *Globals) error {
-	return run(app, cmd.Model, cmd.Path, cmd.Language, cmd.Format, cmd.Segments, false)
+	if cmd.Api {
+		return run_remote(app, cmd.Model, cmd.Path, cmd.Language, cmd.Format, cmd.Segments, false)
+	} else {
+		return run_local(app, cmd.Model, cmd.Path, cmd.Language, cmd.Format, cmd.Segments, false)
+	}
 }
 
 func (cmd *TranslateCmd) Run(app *Globals) error {
-	return run(app, cmd.Model, cmd.Path, "", cmd.Format, cmd.Segments, true)
+	if cmd.Api {
+		return run_remote(app, cmd.Model, cmd.Path, "", cmd.Format, cmd.Segments, true)
+	} else {
+		return run_local(app, cmd.Model, cmd.Path, "", cmd.Format, cmd.Segments, true)
+	}
 }
 
-func run(app *Globals, model, path, language, format string, segments time.Duration, translate bool) error {
+func run_local(app *Globals, model, path, language, format string, segments time.Duration, translate bool) error {
 	// Get the model
 	model_ := app.service.GetModelById(model)
 	if model_ == nil {
@@ -96,6 +114,50 @@ func run(app *Globals, model, path, language, format string, segments time.Durat
 			})
 		}); err != nil {
 			return err
+		}
+
+		return nil
+	})
+}
+
+func run_remote(app *Globals, model, path, language, format string, segments time.Duration, translate bool) error {
+	// Open the audio file
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Create a client for the whisper service
+	remote, err := client.New(remoteUrl)
+	if err != nil {
+		return err
+	}
+
+	// Create a segmenter - read segments based on requested segment size
+	segmenter, err := segmenter.NewReader(f, segments, whisper.SampleRate)
+	if err != nil {
+		return err
+	}
+	defer segmenter.Close()
+
+	// Read samples and transcribe or translate them
+	return segmenter.DecodeFloat32(app.ctx, func(ts time.Duration, buf []float32) error {
+		// Make a WAV file from the float32 samples
+		var wav bytes.Buffer
+
+		if translate {
+			translation, err := remote.Translate(app.ctx, model, &wav)
+			if err != nil {
+				return err
+			}
+			fmt.Println(translation)
+		} else {
+			transcription, err := remote.Transcribe(app.ctx, model, &wav, client.OptLanguage(language))
+			if err != nil {
+				return err
+			}
+			fmt.Println(transcription)
 		}
 
 		return nil
